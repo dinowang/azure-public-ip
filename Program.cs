e894d69f73b7bf1b82ea70f868d2b7c91effee1b
Dino Wang <dino.cool@gmail.com>
2018-12-20 18:06:23 +0800

    updates

--- Program.cs
+++ Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
+using System.Xml;
 using System.Xml.Linq;
 
 namespace azure_public_ip
@@ -15,30 +16,36 @@ namespace azure_public_ip
             var url = "https://download.microsoft.com/download/0/1/8/018E208D-54F8-44CD-AA26-CD7BC9524A8C/PublicIPs_20181217.xml";
 
             var client = new HttpClient();
-            using (var stream = await client.GetStreamAsync(url))
+            var xml = await client.GetStringAsync(url);
+
+            using (var output = new FileStream("output/ip.xml", FileMode.Create, FileAccess.ReadWrite))
+            using (var writer = new StreamWriter(output))
             {
-                var cancellationToken = new CancellationToken();
-                var xdoc = await XDocument.LoadAsync(stream, LoadOptions.None, cancellationToken);
-                var regions = xdoc.Descendants(XName.Get("Region"));
+                writer.WriteLine(xml);
+            }
 
-                var regionIpRanges = regions.Select(x => new 
-                                             { 
-                                                 IpRanges = x.Elements().Select(e => new 
-                                                 {
-                                                     Region = x.Attribute("Name").Value,
-                                                     IpRange = e.Attribute("Subnet").Value
-                                                 })
-                                             })
-                                             .SelectMany(x => x.IpRanges);
+            var cancellationToken = new CancellationToken();
+            var reader = new StringReader(xml);
+            var xdoc = await XDocument.LoadAsync(reader, LoadOptions.None, cancellationToken);
+            var regions = xdoc.Descendants(XName.Get("Region"));
 
-                using (var output = new FileStream("output/ip.txt", FileMode.Create, FileAccess.ReadWrite))
-                using (var writer = new StreamWriter(output))
+            var regionIpRanges = regions.Select(x => new 
+                                            { 
+                                                IpRanges = x.Elements().Select(e => new 
+                                                {
+                                                    Region = x.Attribute("Name").Value,
+                                                    IpRange = e.Attribute("Subnet").Value
+                                                })
+                                            })
+                                            .SelectMany(x => x.IpRanges);
+
+            using (var output = new FileStream("output/ip.csv", FileMode.Create, FileAccess.ReadWrite))
+            using (var writer = new StreamWriter(output))
+            {
+                writer.WriteLine("Region,IpRange");
+                foreach (var ipRange in regionIpRanges)
                 {
-                    writer.WriteLine("Region,IpRange");
-                    foreach (var ipRange in regionIpRanges)
-                    {
-                        writer.WriteLine($"{ipRange.Region},{ipRange.IpRange}");
-                    }
+                    writer.WriteLine($"{ipRange.Region},{ipRange.IpRange}");
                 }
             }
         }
