79efdf561c3d659dffba5cb1bdebb513f26e63f0
Dino Wang <dino.cool@gmail.com>
2018-12-25 14:24:44 +0800

    1. restructure 2. get service tags 3. reserve output files by dates

--- Program.cs
+++ Program.cs
@@ -6,46 +6,89 @@ using System.Threading;
 using System.Threading.Tasks;
 using System.Xml;
 using System.Xml.Linq;
+using HtmlAgilityPack;
 
 namespace azure_public_ip
 {
     class Program
     {
+        static HttpClient _client = new HttpClient();
+
         static async Task Main(string[] args)
         {
-            var url = "https://download.microsoft.com/download/0/1/8/018E208D-54F8-44CD-AA26-CD7BC9524A8C/PublicIPs_20181217.xml";
+            // Microsoft Azure Datacenter IP Ranges
+            await GetMicrosoftUpdateDocument("https://www.microsoft.com/en-us/download/details.aspx?id=41653", async x => await IpRangeToCsv(x));
 
-            var client = new HttpClient();
-            var xml = await client.GetStringAsync(url);
+            // Azure IP Ranges and Service Tags – Public Cloud
+            await GetMicrosoftUpdateDocument("https://www.microsoft.com/en-us/download/details.aspx?id=56519");
+        }
 
-            using (var output = new FileStream("output/ip.xml", FileMode.Create, FileAccess.ReadWrite))
+        static async Task<string> GetMicrosoftUpdateDocument(string refUrl, Func<string, Task> postAction = null)
+        {
+            var html = await _client.GetStringAsync(refUrl);
+
+            var htmlDoc = new HtmlDocument();
+            htmlDoc.LoadHtml(html);
+
+            var downloadButton = htmlDoc.DocumentNode.QuerySelector("a.mscom-link.download-button");
+            var confirmUrl = downloadButton.GetAttributeValue("href", null);
+
+            html = await _client.GetStringAsync($"https://www.microsoft.com/en-us/download/{confirmUrl}");
+
+            htmlDoc = new HtmlDocument();
+            htmlDoc.LoadHtml(html);
+
+            var failoverLink = htmlDoc.DocumentNode.QuerySelector("a.mscom-link.failoverLink");
+            var downloadUrl = failoverLink.GetAttributeValue("href", null);
+
+            var uri = new Uri(downloadUrl);
+            var fileName = Path.GetFileName(uri.PathAndQuery);
+            var outputFileName = $"output/{fileName}";
+            
+            using (var output = new FileStream(outputFileName, FileMode.Create, FileAccess.ReadWrite))
             using (var writer = new StreamWriter(output))
             {
-                writer.WriteLine(xml);
+                var content = await _client.GetStringAsync(downloadUrl);
+                writer.WriteLine(content);
             }
 
-            var cancellationToken = new CancellationToken();
-            var reader = new StringReader(xml);
-            var xdoc = await XDocument.LoadAsync(reader, LoadOptions.None, cancellationToken);
-            var regions = xdoc.Descendants(XName.Get("Region"));
-
-            var regionIpRanges = regions.Select(x => new 
-                                            { 
-                                                IpRanges = x.Elements().Select(e => new 
-                                                {
-                                                    Region = x.Attribute("Name").Value,
-                                                    IpRange = e.Attribute("Subnet").Value
-                                                })
-                                            })
-                                            .SelectMany(x => x.IpRanges);
-
-            using (var output = new FileStream("output/ip.csv", FileMode.Create, FileAccess.ReadWrite))
-            using (var writer = new StreamWriter(output))
+            if (postAction != null)
             {
-                writer.WriteLine("Region,IpRange");
-                foreach (var ipRange in regionIpRanges)
+                await postAction(outputFileName);
+            }
+
+            return outputFileName;
+        }
+
+        public static async Task IpRangeToCsv(string outputFileName)
+        {
+            var cancellationToken = new CancellationToken();
+
+            using (var reader = new StreamReader(outputFileName))
+            {
+                var xdoc = await XDocument.LoadAsync(reader, LoadOptions.None, cancellationToken);
+                var regions = xdoc.Descendants(XName.Get("Region"));
+
+                var regionIpRanges = regions.Select(x => new 
+                                                { 
+                                                    IpRanges = x.Elements().Select(e => new 
+                                                    {
+                                                        Region = x.Attribute("Name").Value,
+                                                        IpRange = e.Attribute("Subnet").Value
+                                                    })
+                                                })
+                                                .SelectMany(x => x.IpRanges);
+
+                var csvFileName = Path.ChangeExtension(outputFileName, ".csv");
+
+                using (var output = new FileStream(csvFileName, FileMode.Create, FileAccess.ReadWrite))
+                using (var writer = new StreamWriter(output))
                 {
-                    writer.WriteLine($"{ipRange.Region},{ipRange.IpRange}");
+                    writer.WriteLine("Region,IpRange");
+                    foreach (var ipRange in regionIpRanges)
+                    {
+                        writer.WriteLine($"{ipRange.Region},{ipRange.IpRange}");
+                    }
                 }
             }
         }
